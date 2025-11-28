7bbe96269b04c2e342e4b5936de6411868a13914
[ATTILA] <98atila@gmail.com>
2025-11-28 20:12:01 +0330

    remove docker

--- src/utils/app-keys.ts
+++ src/utils/app-keys.ts
@@ -1,7 +1,7 @@
-const API_BASE_URL = import.meta.env.VITE_APP_API_BASE_URL;
+const API_BASE_URL = import.meta.env.VITE_API_BASE_URL;
 
 if (!API_BASE_URL) {
-  throw new Error('VITE_APP_API_BASE_URL is not defined');
+  throw new Error('VITE_API_BASE_URL is not defined');
 }
 
 export const APP_KEYS = {
