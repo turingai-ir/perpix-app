d47c9e4aab5d7e45af7fca2ad06b06f31f78a840
[ATTILA] <98atila@gmail.com>
2026-06-24 14:51:40 +0330

    updated ocker

--- src/utils/cookies.ts
+++ src/utils/cookies.ts
@@ -1,21 +1,23 @@
 import Cookies, { type CookieSetOptions } from "universal-cookie";
 
+const defaultCookieOptions: CookieSetOptions = {
+  path: "/",
+  secure: import.meta.env.MODE === "production",
+  sameSite: "lax",
+};
+
 export const cookies = () => {
-  const c = new Cookies(null, {
-    path: "/",
-    secure: import.meta.env.MODE === "production",
-    sameSite: "lax",
-  });
+  const c = new Cookies(null, defaultCookieOptions);
   const get = (name: string) => {
     return c.get(name);
   };
 
   const set = (name: string, value: string, options?: CookieSetOptions) => {
-    return c.set(name, value, options);
+    return c.set(name, value, { ...defaultCookieOptions, ...options });
   };
 
-  const remove = (name: string) => {
-    return c.remove(name);
+  const remove = (name: string, options?: CookieSetOptions) => {
+    return c.remove(name, { ...defaultCookieOptions, ...options });
   };
 
   return { get, set, remove };
