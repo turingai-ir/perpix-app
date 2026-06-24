0d307e5f9227cdc110fb35c4fbd9b12793022fa9
[ATTILA] <98atila@gmail.com>
2026-06-24 18:05:05 +0330

    update env

--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -2,7 +2,7 @@
 
 interface ImportMetaEnv {
   readonly VITE_PERPIXAI_API_URL: string;
-  readonly VITE_S3_PUBLIC_ENDPOINT: string;
+  readonly VITE_OBJECT_STORAGE_PUBLIC_ENDPOINT: string;
   readonly VITE_FONT_BASE_URL?: string;
   readonly VITE_SENTRY_DSN?: string;
   readonly VITE_PERPIX_TAX_PERCENT?: string;
