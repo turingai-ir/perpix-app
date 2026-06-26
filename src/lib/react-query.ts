1a8423eb9db836c9c386b2be254ece070e5361fa
[ATTILA] <98atila@gmail.com>
2026-06-26 23:06:08 +0330

    0.4.9

--- src/lib/react-query.ts
+++ src/lib/react-query.ts
@@ -9,20 +9,19 @@ import { get, set, del } from "idb-keyval";
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
-      staleTime: 1000 * 60,
+      staleTime: 0,
       gcTime: 1000 * 60 * 15,
 
       refetchOnWindowFocus: false,
+      refetchOnMount: "always",
       refetchOnReconnect: true,
 
       retry: 1,
 
-      retryOnMount: true,
-
-      networkMode: "offlineFirst",
+      networkMode: "online",
     },
     mutations: {
-      networkMode: "offlineFirst",
+      networkMode: "online",
     },
   },
 });
